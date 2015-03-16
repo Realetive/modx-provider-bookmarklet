d15b2af7829a0e7aa9deaa4b823cb7249b31935c
Realetive <realetive@yandex.ru>
2015-03-16 11:44:38 +0300

    Create bookmarklet.js

---
+++ bookmarklet.js
@@ -0,0 +1,29 @@
+function(){
+  if (typeof MODx != 'undefined' && typeof MODx.load != 'undefined') {
+    var action = (MODx.action) ? MODx.action['workspaces'] : 'workspaces';
+    if (action == MODx.request.a) {
+      var providerName = prompt('Пожалуйста, обязательно введите название поставщика пакетов (по умолчанию: Simple Dream)', 'Simple Dream')
+        , providerURL  = prompt('Адрес магазина (тоже обязательно)', 'http://modstore.pro/extras/')
+        , userName     = prompt('Имя пользователя', '')
+        , userKey      = prompt('Ключ', '')
+        , desc         = prompt('Описание', 'Магазин дополнений Simple Dream')
+        , win          = MODx.load({xtype : 'modx-window-provider-create'
+                                  , record: {name: providerName
+                                            , description: desc
+                                            , service_url: providerURL
+                                            , username   : userName
+                                            , api_key    : userKey
+                                            }
+                                  });
+      if (providerName != null && providerURL != null) {
+        win.show();
+        win.submit();
+      }
+    } else {
+    alert('Похоже, вы не открыли страницу «Управления пакетами». Это произойдёт самостоятельно; пожалуйста, запустите скрипт ещё раз');
+    MODx.loadPage(action);
+    }
+  } else {
+    alert('Пожалуйста, запустите этот скрипт в панели управления пакетами MODX вашего сайта');
+  }
+}
