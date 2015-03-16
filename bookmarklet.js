5e0073a82cd62c0a075d977b60581ead05349fe7
Realetive <realetive@yandex.ru>
2015-03-16 16:53:59 +0300

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -12,11 +12,11 @@ function addProvider(){
   if (typeof MODx != 'undefined' && typeof MODx.load != 'undefined') {
     var action = (MODx.action) ? MODx.action['workspaces'] : 'workspaces';
     if (action == MODx.request.a) {
-      var providerName = prompt('Пожалуйста, обязательно введите название поставщика пакетов (по умолчанию: Simple Dream)', 'Simple Dream')
+      var providerName = prompt('Пожалуйста, обязательно введите название поставщика пакетов (по умолчанию: modstore.pro)', 'modstore.pro')
         , providerURL  = prompt('Адрес магазина (тоже обязательно)', 'http://modstore.pro/extras/')
         , userName     = prompt('Имя пользователя', '')
         , userKey      = prompt('Ключ', '')
-        , desc         = prompt('Описание', 'Магазин дополнений Simple Dream')
+        , desc         = prompt('Описание', 'Магазин дополнений modstore.pro')
         , win          = MODx.load({xtype : 'modx-window-provider-create'
                                   , record: {name: providerName
                                             , description: desc
