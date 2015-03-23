2d9560cc0a33ed45f608fd99a516fc32c017511d
Realetive <realetive@yandex.ru>
2015-03-23 14:51:14 +0300

    update errors msg

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,39 +1,65 @@
-if (typeof jQuery=='undefined') {
-  var script    = document.createElement('script');
-  script.src    = 'https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';
-  script.onload = addProvider;
-  document.body.appendChild(script);
-}
-else {
-  addProvider();
+var locale = 'en',
+    bmLexicon = {
+      ru: {
+        redirect: 'Похоже, вы не открыли страницу «Управления пакетами». ' +
+                  'Это произойдёт автоматически; пожалуйста, запустите ' +
+                  'скрипт ещё раз'
+      , msgError: 'Пожалуйста, запустите этот скрипт в панели управления ' +
+                  'пакетами MODX вашего сайта'
+      , provider: 'Магазин дополнений modstore.pro'
+      },
+      en: {
+        redirect: 'You don\'t seem to be viewing the Package Management page. ' +
+                  'When you click OK, we will take you there. Please run the ' +
+                  'bookmarklet again once the Package Management page has loaded'
+      , msgError: 'Please run this script from MODX Package Manager on your site'
+      , provider: 'Extras repository modstore.pro'
+      }
 }
 
-function addProvider(){
-  if (typeof MODx != 'undefined' && typeof MODx.load != 'undefined') {
-    var action = (MODx.action) ? MODx.action['workspaces'] : 'workspaces';
-    if (action == MODx.request.a) {
-      var providerName = prompt(_('provider_add') + '. ' + _('field_required') + '. ' + _('tv_default') + ': modstore.pro)', 'modstore.pro')
-        , providerURL  = prompt(_('provider_url') + '. ' + _('field_required') + '.', 'http://modstore.pro/extras/')
-        , userName     = prompt(_('username'), '')
-        , userKey      = prompt(_('api_key'), '')
-        , desc         = prompt(_('description'), 'Магазин дополнений modstore.pro')
-        , win          = MODx.load({xtype : 'modx-window-provider-create'
-                                  , record: {name: providerName
-                                            , description: desc
-                                            , service_url: providerURL
-                                            , username   : userName
-                                            , api_key    : userKey
-                                            }
-                                  });
-      if (providerName != null && providerURL != null) {
+function getLexicon(langKey, locale = 'en') {
+  var langValue = ( bmLexicon[locale].langKey )
+                  ? bmLexicon[locale].langKey
+                  : bmLexicon['en'].langKey
+  return langValue;
+}
+
+function addProvider() {
+  if ( typeof MODx != 'undefined' && typeof MODx.load != 'undefined' ) {
+    var action = ( MODx.action ) ? MODx.action[ 'workspaces' ] : 'workspaces',
+        locale = MODx.config.manager_language == 'ru' ? 'ru' : 'en';
+
+    if ( action == MODx.request.a ) {
+      var providerName = prompt( _( 'provider_add' ) + '. ' + _( 'field_required' ) +
+                                 _( 'tv_default' ) + ': modstore.pro)'
+                                , 'modstore.pro' )
+        , providerURL  = prompt( _( 'provider_url' ) + '. ' + _( 'field_required' )
+                                , 'http://modstore.pro/extras/' )
+        , userName     = prompt( _( 'username' ), '' )
+        , userKey      = prompt( _( 'api_key' ), '' )
+        , desc         = prompt( _( 'description' ), bmLexicon[locale].provider )
+        , win          = MODx.load( { 
+                                      xtype : 'modx-window-provider-create'
+                                    , record: { 
+                                                name       : providerName
+                                              , description: desc
+                                              , service_url: providerURL
+                                              , username   : userName
+                                              , api_key    : userKey
+                                              } } );
+
+      if ( providerName != null && providerURL != null ) {
         win.show();
         win.submit();
+        MODx.loadPage( action );
       }
     } else {
-    alert('Похоже, вы не открыли страницу «Управления пакетами». Это произойдёт самостоятельно; пожалуйста, запустите скрипт ещё раз');
-    MODx.loadPage(action);
+      alert( bmLexicon[locale].redirect );
+      MODx.loadPage( action );
     }
   } else {
-    alert('Пожалуйста, запустите этот скрипт в панели управления пакетами MODX вашего сайта');
+    alert( bmLexicon[locale].msgError );
   }
 }
+
+addProvider();
